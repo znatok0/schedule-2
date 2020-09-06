a476e3824bd264769fb0a206d8dfec42456a98b7
arnurkelgenbaev <galiaskar.uli@gmail.com>
2020-09-06 23:12:07 +0600

    ssad

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
     <header>
         <nav class="navbar navbar-expand-lg navbar-light">
             <div class="header-content">
-                <a class="navbar-brand" href="{{ url('/') }}"><img class="logo" src="/img/index.png" alt="logo"></a>
+                <a class="navbar-brand" href="/"><img class="logo" src="/img/index.png" alt="logo"></a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
